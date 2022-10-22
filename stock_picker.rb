0b2ce56c963122bfd30a0e358fa4ee9b96ef4125
Robert <doubtfullink53@gmail.com>
2022-10-21 22:30:24 -0400

    take out spaces

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,34 +1,20 @@
 
 
 def stock_picker(prices)
-
-  buy_date = []
-  profit = 0
-  mininum_price = prices[0]
-  mininum_index = 0
- 
-  
+  buy_date = [], profit = 0 , mininum_price = prices[0] , mininum_index = 0
 
     prices.each_with_index do |price, index |
 
     subtract_price = price - mininum_price
-
       if ( mininum_price > price)
          mininum_price = price
          mininum_index = index
-         
       elsif (profit <  subtract_price  )
         profit = subtract_price
         buy_date = [mininum_index, index]
       end
     end
-
-    
   buy_date.join(" , ")
-  
-
-    
-   
   end
   
 
